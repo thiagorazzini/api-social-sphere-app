db0abd49ccebb9f4bcf09cb1edb4842d9f27e1d1
Thiago Razzini <razzini.thiago@gmail.com>
2024-06-17 22:03:46 -0300

    Created a endpoint to register new account

---
+++ src/SocialSphereApp.API/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialSphereApp.API.Models;
+
+namespace SocialSphereApp.API.Controllers
+{
+    [Route("api/v1/accounts")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        // POST api/v1/register
+        [HttpPost]
+        public IActionResult RegisterNewAccount(CreateAccountInputModel model)
+        {
+            return CreatedAtAction(nameof(GetAccountById), new { id = 1 }, model);
+        }
+
+        // GET api/v1/accounts/1234
+        [HttpGet("{id}")]
+        public IActionResult GetAccountById(Guid accountId)
+        {
+            return Ok(accountId);
+        }
+    }
+}
---
+++ src/SocialSphereApp.API/Models/CreateAccountInputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialSphereApp.API.Models
+{
+    public class CreateAccountInputModel
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string Mail { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Phone { get; set; }
+    }
+}
